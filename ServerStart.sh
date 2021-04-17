45af2ef38dd3b9f750e7ca187dce578ec923efd8
LionZXY <nikita@kulikof.ru>
2021-04-17 14:08:47 +0300

    Initial commit

---
+++ ServerStart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+HOST="https://games.glitchless.ru/api/minecraft/users"
+#HOST="https://mc-gls.free.beeceptor.com"
+#HOST="https://localhost/api/minecraft/users/"
+java -Dminecraft.api.auth.host=$HOST \
+	  -Dminecraft.api.account.host=$HOST \
+	    -Dminecraft.api.session.host=$HOST \
+	      -Dminecraft.api.services.host="https://games.glitchless.ru/api/minecraft/" \
+                  -jar -Xmx32G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch forge-1.16.5-36.0.43.jar nogui
